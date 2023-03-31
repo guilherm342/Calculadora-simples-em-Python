ce85f00907cfcd2c7d62d1095738aaca24eca303
Guilherme Freitas <91505334+guilherm342@users.noreply.github.com>
2023-03-30 22:49:48 -0300

    Create calculadorasimples.py

---
+++ calculadorasimples.py
@@ -0,0 +1,23 @@
+#calculadora simples
+
+numero1 = 0
+numero2 = 0
+resultado = 0
+operacao = ''
+
+numero1 = int(input("digite o primeiro numero: "))
+operacao = input("digite a operação: ")
+numero2 = int(input("digite o segundo numero: "))
+
+if operacao == '+':
+    resultado = numero1 + numero2
+elif operacao == '-':
+     resultado = numero1 - numero2
+elif operacao == '/':
+     resultado = numero1 / numero2
+elif operacao == '*':
+     resultado = numero1 * numero2
+else: 'operação invalida'
+
+
+print(str(numero1) + '' + str(operacao) + '' + str(numero2) + ' = ' + str(resultado))
